ab1db36a8c4844d54676f996de8ca011c92ec2b5
Ansh Parmar <153165266+REKT-Ap@users.noreply.github.com>
2024-02-08 15:54:39 +0530

    Add files via upload

---
+++ Bootstrape-Examples/bs5.js
@@ -0,0 +1,15 @@
+/*  For ToolTip : */
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+/* For popovers */
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
